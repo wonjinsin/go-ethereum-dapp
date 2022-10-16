ae9652c7c900af24d3d10dc26b032cd6dcb9c2cf
Wonjinsin <slgktn92@gmail.com>
2022-10-17 00:18:56 +0900

    add. simple coin solidity transfer

--- contract/simpleCoin.sol
+++ contract/simpleCoin.sol
@@ -2,14 +2,17 @@ pragma solidity ^0.4.0;
 
 contract SimpleCoin {
     mapping (address => uint256) public coinBalance;
+    event Transfer(address indexed from, address indexed to, uint256 value);
 
-    constructor() public {
-        coinBalance
-        [0x5B38Da6a701c568545dCfcB03FcB875f56beddC4] = 10000;
+    constructor(uint256 _initialSupply) public {
+        coinBalance[msg.sender] = _initialSupply;
     }
 
     function transfer(address _to, uint256 _amount) public {
+        require(coinBalance[msg.sender] >= _amount);
+        require(coinBalance[_to] + _amount >= coinBalance[_to]);
         coinBalance[msg.sender] -= _amount;
         coinBalance[_to] += _amount;
+        emit Transfer(msg.sender, _to, _amount);
     }
 }
